c3f1d6f96ccb6f1ab9f8d61e9d23baa0b31acb73
Tiến Đạt <datpro7703@gmail.com>
2024-07-01 09:09:51 +0700

    Update main.go fir interface
    Update main.go fir interface

--- Projects/interfaces/main.go
+++ Projects/interfaces/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+type bot interface {
+	greeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	greeting(eb)
+	greeting(sb)
+}
+
+func greeting(b bot) {
+	fmt.Println(b.greeting())
+}
+
+func (englishBot) greeting() string {
+	return "Hello there!"
+}
+
+func (spanishBot) greeting() string {
+	return "Hola!"
 }
